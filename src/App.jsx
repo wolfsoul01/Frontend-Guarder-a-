720d05b1f5b2eeacba8d4e9245fe8cbcdfaa8b11
wolfsoul <alejandrof011021@gmail.com>
2023-10-17 11:10:04 -0400

    Clear proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
-import './App.css'
+import {Home} from './components/Home'
 
 function App() {
   
 
   return (
     <>
-     <h1 className='text-5xl text-center font-bold pt-7' >Hola mundo</h1>
+     <Home/>
     </>
   )
 }
