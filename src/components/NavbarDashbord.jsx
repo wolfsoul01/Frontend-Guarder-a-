e5f86dd70195abf5e17613bf550434d1808793f6
wolfsoul <alejandrof011021@gmail.com>
2023-10-23 13:30:31 -0400

    Update navbar delete links

--- src/components/NavbarDashbord.jsx
+++ src/components/NavbarDashbord.jsx
@@ -14,30 +14,7 @@ export function Navbar () {
               Dashbord
             </span>
           </a>
-          <button
-            data-collapse-toggle='navbar-dropdown'
-            type='button'
-            className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-            aria-controls='navbar-dropdown'
-            aria-expanded='false'
-          >
-            <span className='sr-only'>Open main menu</span>
-            <svg
-              className='w-5 h-5'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 17 14'
-            >
-              <path
-                stroke='currentColor'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M1 1h15M1 7h15M1 13h15'
-              />
-            </svg>
-          </button>
+        
           <div
             className='hidden w-full md:block md:w-auto'
             id='navbar-dropdown'
@@ -52,97 +29,16 @@ export function Navbar () {
                   Ninos
                 </NavLink>
               </li>
-              <li>
-                <button
-                  id='dropdownNavbarLink'
-                  data-dropdown-toggle='dropdownNavbar'
-                  className='flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent'
-                >
-                  Dropdown{' '}
-                  <svg
-                    className='w-2.5 h-2.5 ml-2.5'
-                    aria-hidden='true'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 10 6'
-                  >
-                    <path
-                      stroke='currentColor'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='2'
-                      d='m1 1 4 4 4-4'
-                    />
-                  </svg>
-                </button>
-
-                <div
-                  id='dropdownNavbar'
-                  className='z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600'
-                >
-                  <ul
-                    className='py-2 text-sm text-gray-700 dark:text-gray-400'
-                    aria-labelledby='dropdownLargeButton'
-                  >
-                    <li>
-                      <a
-                        href='#'
-                        className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                      >
-                        Dashboard
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='#'
-                        className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                      >
-                        Settings
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='#'
-                        className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                      >
-                        Earnings
-                      </a>
-                    </li>
-                  </ul>
-                  <div className='py-1'>
-                    <a
-                      href='#'
-                      className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white'
-                    >
-                      Sign out
-                    </a>
-                  </div>
-                </div>
-              </li>
+             
               <li>
                 <NavLink
                   to={'/dashbord/master'}
                   className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 >
-                  Master
+                  Maestros
                 </NavLink>
               </li>
-              <li>
-                <a
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  Contact
-                </a>
-              </li>
+             
             </ul>
           </div>
         </div>
