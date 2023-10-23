62dc3960c2aed6c46088b62a8dbd5da7e6760f40
wolfsoul <alejandrof011021@gmail.com>
2023-10-23 08:55:33 -0400

    Intall react router and flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
@@ -14,5 +16,7 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    import('flowbite/plugin')
+  ],
 }
