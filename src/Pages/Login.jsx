1576edc908b991c3610b44cfd3ec60be5447fd21
wolfsoul <alejandrof011021@gmail.com>
2023-10-23 11:32:40 -0400

    Update text in login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,7 +5,7 @@ function Login(){
 <section className="relative flex flex-wrap lg:h-screen lg:items-center">
   <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
     <div className="mx-auto max-w-lg text-center">
-      <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
+      <h1 className="text-2xl font-bold sm:text-3xl">Inicia Sesion </h1>
 
       <p className="mt-4 text-gray-500">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
