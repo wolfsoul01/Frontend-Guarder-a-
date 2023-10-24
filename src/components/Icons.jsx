4276856c46ffcb5f1ddc4d8de7718ace07eec7a5
wolfsoul <alejandrof011021@gmail.com>
2023-10-23 21:03:49 -0400

    Create responsi navbar Dasbord

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -5,12 +5,11 @@ export function HambMenu ({ handleClick }) {
       <label
         htmlFor='check'
         className='flex flex-col cursor-pointer w-[40px] md:hidden'
-        
       >
-        <input type='checkbox' name='' id='check' onClick={handleClick}/>
-        <span className='row'></span>
-        <span className='row'></span>
-        <span className='row'></span>
+        <input type='checkbox' name='' id='check' onClick={handleClick} />
+          <span className='row'></span>
+          <span className='row'></span>
+          <span className='row'></span>
       </label>
     </>
   )
--- src/components/NavbarDashbord.jsx
+++ src/components/NavbarDashbord.jsx
@@ -1,39 +1,45 @@
 import { NavLink } from 'react-router-dom'
 import { HambMenu } from './Icons'
+import { useState } from 'react'
 export function Navbar () {
+
+  const [isOpen,setisOpen]=useState(false)
+
+  const handleClick=()=>{
+    setisOpen(!isOpen)
+  }
+
   return (
     <>
-      <nav className='bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700'>
+
+
+      <nav className='bg-white  dark:bg-gray-900 dark:border-gray-700'>
         <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-          <a href='#' className='flex items-center'>
-            <img
-              src='https://flowbite.com/docs/images/logo.svg'
-              className='h-8 mr-3'
-              alt='Flowbite Logo'
-            />
-            <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-              Dashbord
-            </span>
-          </a>
+          <header className=' flex justify-between w-full md:w-fit'>
+            <a href='#' className='flex items-center'>
+              <img
+                src='https://flowbite.com/docs/images/logo.svg'
+                className='h-8 mr-3'
+                alt='Flowbite Logo'
+              />
+              <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+                Dashbord
+              </span>
+            </a>
 
-          <HambMenu />
+            <HambMenu handleClick={handleClick} />
+          </header>
 
-          <div className='   '>
-            <ul className='md:flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+          <div className={`${!isOpen ? 'hidden':''} md:block`}>
+            <ul className='text-white flex flex-col md:flex-row gap-5  flex-grow  '>
               <li>
-                <NavLink
-                  to={'children'}
-                  className='block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 md:dark:bg-transparent'
-                >
+                <NavLink to={'children'} className=''>
                   Ninos
                 </NavLink>
               </li>
 
               <li>
-                <NavLink
-                  to={'/dashbord/master'}
-                  className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
+                <NavLink to={'/dashbord/master'} className=''>
                   Maestros
                 </NavLink>
               </li>
