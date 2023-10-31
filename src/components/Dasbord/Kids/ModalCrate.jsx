72c200a6cb956e800ade6e04d082e3fec66a3951
wolfsoul <alejandrof011021@gmail.com>
2023-10-31 12:57:27 -0400

    Retoques al modal de cracion

--- src/components/Dasbord/Kids/ModalCrate.jsx
+++ src/components/Dasbord/Kids/ModalCrate.jsx
@@ -2,36 +2,36 @@
 import { Button, Modal } from 'flowbite-react'
 import { Label, TextInput } from 'flowbite-react'
 
-export default function ModalCreate ({ openModal, handleModal, setKids,kids }) {
+export default function ModalCreate ({ openModal, handleModal, setKids, kids }) {
   function handleCreate (e) {
     e.preventDefault()
 
-    const info =Object.fromEntries( new FormData(e.target))
+    const info = Object.fromEntries(new FormData(e.target))
 
-    const newKid={
-        id: new Date,
-        nombre: info.name,
-        apellidos: info.apellido,
-        fechaNacimiento: info.fechaNacimiento,
-        edad: info.edad,
-        aula: info.aula,
-        estado: false,
-        alergias: info.alergias,
-        contactoEmergencia: {
-          nombre: info.contactoSOS,
-          telefono: info.contactoTelefono,
-          parentesco: info.contactoParentesco
-        }
+    const newKid = {
+      id: new Date(),
+      nombre: info.name,
+      apellidos: info.apellido,
+      fechaNacimiento: info.fechaNacimiento,
+      edad: info.edad,
+      aula: info.aula,
+      estado: true,
+      alergias: info.alergias,
+      contactoEmergencia: {
+        nombre: info.contactoSOS,
+        telefono: info.contactoTelefono,
+        parentesco: info.contactoParentesco
+      }
     }
 
-    setKids([...kids,newKid])
+    setKids([...kids, newKid])
     handleModal()
   }
 
   return (
     <>
       <Modal show={openModal} onClose={handleModal}>
-        <Modal.Header>Editar Informacion</Modal.Header>
+        <Modal.Header>Add</Modal.Header>
         <form action='' onSubmit={handleCreate}>
           <Modal.Body className='grid grid-cols-2 gap-x-3'>
             <div className='flex max-w-md flex-col gap-2'>
@@ -48,14 +48,16 @@ export default function ModalCreate ({ openModal, handleModal, setKids,kids }) {
                 placeholder='Ingresa el apellido'
                 type='text'
                 id='apellidoInput'
+                required
                 name='apellido'
               />
               <Label htmlFor='fechaNacimientoInput'>Fecha de nacimiento</Label>
               <TextInput
                 placeholder='Ingresa la fecha de nacimiento'
-                type='text'
+                type='date'
                 id='fechaNacimientoInput'
                 name='fechaNacimiento'
+                required
               />
               <Label htmlFor='aulaInput'>Aula</Label>
               <TextInput
@@ -63,15 +65,17 @@ export default function ModalCreate ({ openModal, handleModal, setKids,kids }) {
                 type='text'
                 id='aulaInput'
                 name='aula'
+                required
               />
             </div>
             <div className='flex max-w-md flex-col gap-2'>
               <Label htmlFor='edadInput'>Edad</Label>
               <TextInput
                 placeholder='Ingresa la edad'
-                type='text'
+                type='number'
                 id='edadInput'
                 name='edad'
+                required
               />
               <Label htmlFor='alergiasInput'>Alergias</Label>
               <TextInput
@@ -92,6 +96,7 @@ export default function ModalCreate ({ openModal, handleModal, setKids,kids }) {
                       type='text'
                       id='contactoNombreInput'
                       name='contactoSOS'
+                      required
                     />
                   </Label>
                   <Label htmlFor='contactoParentescoInput'>
@@ -100,6 +105,7 @@ export default function ModalCreate ({ openModal, handleModal, setKids,kids }) {
                       placeholder='Ingresa el parentesco del contacto de emergencia'
                       type='text'
                       id='contactoParentescoInput'
+                      required
                       name='contactoParentesco'
                     />
                   </Label>
@@ -107,9 +113,9 @@ export default function ModalCreate ({ openModal, handleModal, setKids,kids }) {
                     Telefono
                     <TextInput
                       placeholder='Ingresa el teléfono del contacto de emergencia'
-                      readOnly
-                      type='text'
+                      type='number'
                       id='contactoTelefonoInput'
+                      required
                       name='contactoTelefono'
                     />
                   </Label>
