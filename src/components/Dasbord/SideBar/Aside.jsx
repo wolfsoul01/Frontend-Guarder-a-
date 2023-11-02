3740851836296ac6137204c04181cc09d85b16ab
wolfsoul <alejandrof011021@gmail.com>
2023-11-02 13:52:15 -0400

    Error en en path de la rutas :)

--- src/components/Dasbord/SideBar/Aside.jsx
+++ src/components/Dasbord/SideBar/Aside.jsx
@@ -60,7 +60,7 @@ export function AsideBar ({ isOpen }) {
               >
                 <li>
                   <NavLink
-                    to={''}
+                    to={'kids'}
                     className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group '
                   >
                     <svg
