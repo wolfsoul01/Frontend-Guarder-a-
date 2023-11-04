2a2708e677346e23f4a4cf646b9018150d9a8262
wolfsoul <alejandrof011021@gmail.com>
2023-11-04 07:09:17 -0400

    Add new routes grap to sidebar

--- src/components/Dasbord/SideBar/Aside.jsx
+++ src/components/Dasbord/SideBar/Aside.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { Users } from '../../Icons'
 // eslint-disable-next-line react/prop-types
 export function AsideBar ({ isOpen }) {
-  const [isOpenSubMenu, setOpenSubMenu] = useState(false)
+  const [isOpenSubMenuTab, setOpenSubMenu] = useState(false)
+  const [isOpenSubMenuGrap, setOpenSubMenuGeap] = useState(false)
 
-  function handleClick () {
-    setOpenSubMenu(!isOpenSubMenu)
-  }
+ 
 
   return (
     <>
@@ -22,23 +22,14 @@ export function AsideBar ({ isOpen }) {
               <button
                 type='button'
                 className='flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700'
-                onClick={handleClick}
+                onClick={()=> setOpenSubMenu(!isOpenSubMenuTab) }
               >
-                <svg
-                  className='w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='currentColor'
-                  viewBox='0 0 22 21'
-                >
-                  <path d='M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z' />
-                  <path d='M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z' />
-                </svg>
+               <Users/>
                 <span className='flex-1 ml-3 text-left whitespace-nowrap'>
                   Dashboard
                 </span>
                 <svg
-                  className='w-3 h-3'
+                   className={`w-3 h-3 ${isOpenSubMenuTab && 'rotate-180'} transition-transform duration-150`}
                   aria-hidden='true'
                   xmlns='http://www.w3.org/2000/svg'
                   fill='none'
@@ -55,7 +46,7 @@ export function AsideBar ({ isOpen }) {
               </button>
               <ul
                 className={`${
-                  isOpenSubMenu ? 'block' : 'hidden'
+                  isOpenSubMenuTab ? 'block' : 'hidden'
                 } py-2 space-y-2`}
               >
                 <li>
@@ -115,10 +106,70 @@ export function AsideBar ({ isOpen }) {
                 </li>
               </ul>
             </li>
+            <li>
+              <button
+                type='button'
+                className='flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700'
+                onClick={()=> setOpenSubMenuGeap(!isOpenSubMenuGrap)}
+              >
+                <svg
+                  className='w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
+                  aria-hidden='true'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='currentColor'
+                  viewBox='0 0 22 21'
+                >
+                  <path d='M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z' />
+                  <path d='M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z' />
+                </svg>
+                <span className='flex-1 ml-3 text-left whitespace-nowrap'>
+                  Graficas
+                </span>
+                <svg
+                  className={`w-3 h-3 ${isOpenSubMenuGrap && 'rotate-180'} transition-transform duration-150`}
+                  aria-hidden='true'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 10 6'
+                >
+                  <path
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='m1 1 4 4 4-4'
+                  />
+                </svg>
+              </button>
+              <ul
+                className={`${
+                  isOpenSubMenuGrap ? 'block' : 'hidden'
+                } py-2 space-y-2`}
+              >
+                <li>
+                  <NavLink
+                    to={'graphi'}
+                    className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group '
+                  >
+                    <svg
+                      className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
+                      aria-hidden='true'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='currentColor'
+                      viewBox='0 0 20 18'
+                    >
+                      <path d='M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z' />
+                    </svg>
+                    <span className='flex-1 ml-3 whitespace-nowrap'>Administracion </span>
+                  </NavLink>
+                </li>
+                
+              </ul>
+            </li>
 
             <li>
-              <a
-                href='#'
+              <NavLink
+              to={'/'}
                 className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
               >
                 <svg
@@ -137,7 +188,7 @@ export function AsideBar ({ isOpen }) {
                   />
                 </svg>
                 <span className='flex-1 ml-3 whitespace-nowrap'>Sign In</span>
-              </a>
+              </NavLink>
             </li>
             <li>
               <a
