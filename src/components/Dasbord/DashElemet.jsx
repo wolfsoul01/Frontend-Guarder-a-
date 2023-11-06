4ce873293ba2ec2d9170733f37570a426d84a4bc
wolfsoul <alejandrof011021@gmail.com>
2023-11-06 12:59:27 -0500

    Udpate styles dashbord seccion

--- src/components/Dasbord/DashElemet.jsx
+++ src/components/Dasbord/DashElemet.jsx
@@ -9,18 +9,22 @@ export function DashElemet ({ Component }) {
   return (
     <>
       <section>
-        <aside className=' grid grid-cols-3 gap-6 text-black '>
-          <article className='bg-dashboard-200 rounded-md grid place-content-center p-3'>
-            <h1>Info</h1>
-            <p>{} </p>
-          </article>
-          <article className='bg-dashboard-200 rounded-md grid place-content-center p-3'>
-            <h1>Info</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-          </article>
-          <article className='bg-dashboard-200 rounded-md grid place-content-center p-3'>
-            <h1>Info</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+        <aside className=' grid grid-cols-2 gap-6 text-black '>
+          <article className='bg-dashboard-200 rounded-md grid  p-3 col-span-2 relative pb-36'>
+            <header className='flex flex-col'>
+              <h1 className='font-semibold text-gray-600 text-xl'>Bienevendio de Vuelta</h1>
+            </header>
+            <aside>
+            <input type="date" value={new Date().toISOString().slice(0,10)} className=' rounded-lg bg-violet-600 text-white' />
+
+            </aside>
+
+            <img
+              src='/img/dasbord.png'
+              alt=''
+              className='absolute right-0 -top-16 '
+              width={300}
+            />
           </article>
         </aside>
 
