26511e7f6eebbced9e312149db5a09a890c07f1a
wolfsoul <alejandrof011021@gmail.com>
2023-11-09 14:46:42 -0500

    add the new palet colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,31 @@ import flow from 'flowbite/plugin'
 export default {
   darkMode: 'class',
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
-      backgroundColor:{
-        primary:"#FFD700",
-        dashboard:{
-          100:'#FFF4F4',
-          200:'#F7E6C4',
-          300:'#F1C376',
-          400:'#606C5D',
+      backgroundColor: {
+        primary: '#FFD700',
+        dashboard: {
+          100: '#FFF4F4',
+          200: '#F7E6C4',
+          300: '#F1C376',
+          400: '#606C5D'
+        },
+        dashboard2: {
+          100: '#CCD0CF',
+          200: '#9BA8AB',
+          300: '#253745',
+          400: '#06141B'
         }
       },
-      textColor:{
-        primary:"#FFD700"
+      textColor: {
+        primary: '#FFD700'
       }
-    },
+    }
   },
-  plugins: [
-   flow
-  ],
+  plugins: [flow]
 }
